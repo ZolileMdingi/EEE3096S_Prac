07a57426797a40c00310a0717fec084116d639cc
ZolileMdingi <50634614+ZolileMdingi@users.noreply.github.com>
2021-09-05 15:44:05 -0700

    Update ES2EEPROMUtils.py

--- Prac-03/ES2EEPROMUtils.py
+++ Prac-03/ES2EEPROMUtils.py
@@ -127,12 +127,12 @@ class ES2EEPROM:
             data_to_write.append(score[1])
         print(data_to_write)
         self.write_block(1, data_to_write)
-        print(eeprom.read_block(1,4))
-        print(self.read_byte(0))
+        
 
 
 if __name__ == "__main__":
     eeprom = ES2EEPROM()
     eeprom.clear(4096)
     eeprom.populate_mock_scores()
+    print(eeprom.read_block(1,8))
 
