6d0925affc6d1bca002157dcb638c43b63721c10
ZolileMdingi <50634614+ZolileMdingi@users.noreply.github.com>
2021-09-05 15:46:59 -0700

    Update ES2EEPROMUtils.py

--- Prac-03/ES2EEPROMUtils.py
+++ Prac-03/ES2EEPROMUtils.py
@@ -134,5 +134,5 @@ if __name__ == "__main__":
     eeprom = ES2EEPROM()
     eeprom.clear(4096)
     eeprom.populate_mock_scores()
-    print(eeprom.read_block(1,8))
+    print(eeprom.read_block(1,4))
 
