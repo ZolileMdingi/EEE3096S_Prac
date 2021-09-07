c65aa1d60ff1e6a57d47ec4be9036fcaf615e509
zolile <amdingi8@gmail.com>
2021-09-07 05:37:08 -0700

    added something new

--- Prac-03/ES2EEPROMUtils.py
+++ Prac-03/ES2EEPROMUtils.py
@@ -138,7 +138,7 @@ def fetch_scores(eeprom):
     score_count = eeprom.read_block(0x00,1)
     print("score_count",score_count)
     
-    scores_raw = eeprom.read_block(1,score_count)
+    scores_raw = eeprom.read_block(1,score_count[0])
     print("the raw scores",scores_raw)
     scores = []
     for x in range(0, score_count*4,4):
