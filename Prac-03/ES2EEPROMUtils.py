7df180efe1b90ff4fc7f72e09f1e8d9f8e43ee2d
ZolileMdingi <50634614+ZolileMdingi@users.noreply.github.com>
2021-09-07 02:11:51 +0200

    Update ES2EEPROMUtils.py

--- Prac-03/ES2EEPROMUtils.py
+++ Prac-03/ES2EEPROMUtils.py
@@ -135,7 +135,7 @@ def getName(nameChars):
         name = name + chr(x)
     return name
 def fetch_scores(eeprom):
-    score_count = eeprom.read_block(0,1)
+    score_count = eeprom.read_byte(0xff)
     print("score_count",score_count)
     score_count = 8
     scores_raw = eeprom.read_block(1,32)
