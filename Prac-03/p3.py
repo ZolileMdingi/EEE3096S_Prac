67573892a9479e245deca2925531a1c4e21c009c
ZolileMdingi <50634614+ZolileMdingi@users.noreply.github.com>
2021-09-07 05:44:50 +0200

    Update p3.py

--- Prac-03/p3.py
+++ Prac-03/p3.py
@@ -213,6 +213,8 @@ def addScore(scores, newScore):
 # Guess button
 def btn_guess_pressed(channel):
     global number_of_tries
+    global _guess
+    global value
     start_time = time.time()
     while GPIO.input(channel) == 0: # Wait for the button up
         pass
