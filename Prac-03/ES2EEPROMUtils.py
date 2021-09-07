8fe7c9e9250ea295a0142f40b4f8d4758bbd406a
ZolileMdingi <50634614+ZolileMdingi@users.noreply.github.com>
2021-09-07 02:19:35 +0200

    Update ES2EEPROMUtils.py

--- Prac-03/ES2EEPROMUtils.py
+++ Prac-03/ES2EEPROMUtils.py
@@ -116,7 +116,7 @@ class ES2EEPROM:
         """
 
         # First 4 bytes contain how many scores there are
-        self.write_block(0, [8])
+        self.write_block(0xff, [8])
         scores = [["jtf", 9], ["dev", 10], ["Lju", 3], ["EuE", 6],["jth", 1], ["hjk", 11], ["xju", 4], ["hgc", 2]]
         scores.sort(key=lambda x: x[1])
         print(scores)
@@ -127,7 +127,7 @@ class ES2EEPROM:
                 data_to_write.append(ord(letter))
             data_to_write.append(score[1])
         print(data_to_write)
-        self.write_block(1, data_to_write)
+        self.write_block(0, data_to_write)
         
 def getName(nameChars):
     name = ''
@@ -135,7 +135,7 @@ def getName(nameChars):
         name = name + chr(x)
     return name
 def fetch_scores(eeprom):
-    score_count = eeprom.read_byte(0)
+    score_count = eeprom.read_byte(0xff)
     print("score_count",score_count)
     score_count = 8
     scores_raw = eeprom.read_block(1,32)
