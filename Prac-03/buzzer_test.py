ee51dbe56e8ae64e45cdc6815685b30aa1e34380
ZolileMdingi <mdnave001@myuct.ac.za>
2021-09-08 23:56:42 +0200

    added update

--- Prac-03/buzzer_test.py
+++ Prac-03/buzzer_test.py
@@ -3,55 +3,55 @@ from gpiozero.tones import Tone
 from gpiozero import TonalBuzzer
 from time import sleep
 import math
-# GPIO.setwarnings(False)
-BuzzerPin = 13
-# buzzerPin = 33
-# buttonPin = 18
-# p = None
-# def setup():
-#   global buzzerPin
-#   global buttonPin
-#   global p
-#   print ('Program is starting...')
-#   GPIO.setmode(GPIO.BOARD)
-#   GPIO.setup(buzzerPin, GPIO.OUT)
-#   p = GPIO.PWM(buzzerPin, 1)
-#   GPIO.setup(buttonPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-#   p.start(0)
+GPIO.setwarnings(False)
+# BuzzerPin = 13
+buzzerPin = 33
+buttonPin = 18
+p = None
+def setup():
+  global buzzerPin
+  global buttonPin
+  global p
+  print ('Program is starting...')
+  GPIO.setmode(GPIO.BOARD)
+  GPIO.setup(buzzerPin, GPIO.OUT)
+  p = GPIO.PWM(buzzerPin, 1)
+  GPIO.setup(buttonPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+  p.start(0)
 
-# def alertor():
-#   p.start(50)
-#   for x in range(0,361):
-#     sinVal = math.sin(x * (math.pi / 180.0))    
-#     toneVal = 2000 + sinVal * 500
-#     p.ChangeFrequency(toneVal)
-#     time.sleep(0.001)
+def alertor():
+  p.start(50)
+  for x in range(0,361):
+    sinVal = math.sin(x * (math.pi / 180.0))    
+    toneVal = 2000 + sinVal * 500
+    p.ChangeFrequency(toneVal)
+    sleep(0.001)
 
-# def stopAlertor():
-#   p.stop()
+def stopAlertor():
+  p.stop()
 
 
-# def loop():
-#   while True:
-#     if GPIO.input(buttonPin)==GPIO.LOW:
-#       alertor()
-#       print ('buzzer on ...')
-#     else :
-#       stopAlertor()
-#       print ('buzzer off ...')
-# def destroy():
-#   GPIO.output(buzzerPin, GPIO.LOW)
-#   GPIO.cleanup()
-# if __name__ == '__main__':
-#   setup()
-#   try:
-#     loop()
-#   except KeyboardInterrupt:
-#     destroy()
+def loop():
+  while True:
+    if GPIO.input(buttonPin)==GPIO.LOW:
+      alertor()
+      print ('buzzer on ...')
+    else :
+      stopAlertor()
+      print ('buzzer off ...')
+def destroy():
+  GPIO.output(buzzerPin, GPIO.LOW)
+  GPIO.cleanup()
+if __name__ == '__main__':
+  setup()
+  try:
+    loop()
+  except KeyboardInterrupt:
+    destroy()
 
-def beep(s):
-    tb = TonalBuzzer(BuzzerPin)
-    tb.play(Tone(frequency=880))
-    sleep(s)
-    tb.stop()
-    tb.close()
+# def beep(s):
+#     tb = TonalBuzzer(BuzzerPin)
+#     tb.play(Tone(frequency=880))
+#     sleep(s)
+#     tb.stop()
+#     tb.close()
