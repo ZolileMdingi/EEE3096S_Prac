cf57f24bd99738aa532284ba23518309bec8b63d
ZolileMdingi <mdnave001@myuct.ac.za>
2021-09-09 00:25:34 +0200

    added

--- Prac-03/buzzer_test.py
+++ Prac-03/buzzer_test.py
@@ -15,17 +15,14 @@ def setup():
   print ('Program is starting...')
   GPIO.setmode(GPIO.BOARD)
   GPIO.setup(buzzerPin, GPIO.OUT)
-  p = GPIO.PWM(buzzerPin, 100)
+  p = GPIO.PWM(buzzerPin, 1000)
   GPIO.setup(buttonPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
   p.start(0)
+  sleep(0.5)
 
 def alertor():
-  p.start(50)
-  for x in range(0,361):
-    sinVal = math.sin(x * (math.pi / 180.0))    
-    toneVal = 2000 + sinVal * 500
-    p.ChangeFrequency(toneVal)
-    sleep(0.001)
+  p.start(100)
+  p.ChangeDutyCycle(100)
 
 def stopAlertor():
   p.stop()
