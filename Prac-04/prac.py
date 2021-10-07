8904a3bca78cf4787c3af644649ed9d3e6edee89
ZolileMdingi <50634614+ZolileMdingi@users.noreply.github.com>
2021-10-07 22:00:07 +0200

    made minor print changes

--- Prac-04/prac.py
+++ Prac-04/prac.py
@@ -7,31 +7,28 @@ from adafruit_debouncer import Debouncer
 import threading
 import adafruit_mcp3xxx.mcp3008 as MCP
 from adafruit_mcp3xxx.analog_in import AnalogIn
+
+
 spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
 cs = digitalio.DigitalInOut(board.D8)
+
 button = digitalio.DigitalInOut(board.D16)
 button.direction = digitalio.Direction.INPUT
 button.pull = digitalio.Pull.UP
 switch = Debouncer(button)
+stop_thread = False
 mcp = MCP.MCP3008(spi, cs)
 lock = threading.Lock()
+
 ldr_channel = AnalogIn(mcp, MCP.P2)
 temp_channel = AnalogIn(mcp, MCP.P1)
 seconds_change = 10
 
-def button_setup(pin):
-   # GPIO.setmode(GPIO.BOARD)
 
-    GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-    GPIO.add_event_detect(pin,GPIO.FALLING, callback=change_secs, bouncetime=500)
-
-
-def temp_in_C(raw_temp,voltageVal):
+def temp_in_C(voltageVal):
     ADC_VREF = 5
-    ADC_V_PER_COUNT = ADC_VREF/1024
     MCP9700_T_COEFF = 0.01
     MCP9700_OFFSET = 0.5
-    voltage = raw_temp*ADC_V_PER_COUNT
     return (voltageVal-MCP9700_OFFSET)/MCP9700_T_COEFF
 
 
@@ -40,11 +37,13 @@ def run_thread():
      thread.start()
 def app(temp_channel, ldr_channel):
     global seconds_change
+    global stop_thread
     second = 0
     init_second_val = time.time()
     print("{} \t {} \t {}   \t {}".format("Runtime","Temp Reading","Temp","Light Reading"))
-    while(True):       
-        print("{runningtime:<}s \t {temp_reading:<9.2f} \t {temmp:<4.2f} C \t {l_reading:<9.2f}".format(runningtime = round(second),temp_reading = temp_channel.value,temmp = temp_in_C(temp_channel.value,temp_channel.voltage),l_reading  = ldr_channel.value))
+    while not stop_thread:
+        print(str(round(second)).ljust(6),'\t','{:<9.2f}'.format(temp_channel.value),'\t','{:<4.2f}'.format(temp_in_C(temp_channel.voltage)),'\t',"{l_reading:<9.2f}".format(ldr_channel.value))       
+        # print("{runningtime:<}s \t {temp_reading:<9.2f} \t {temmp:<4.2f} C \t {l_reading:<9.2f}".format(runningtime = round(second),temp_reading = temp_channel.value,temmp = temp_in_C(temp_channel.value,temp_channel.voltage),l_reading  = ldr_channel.value))
         with lock:
             time.sleep(seconds_change)
             second =time.time() - init_second_val
@@ -54,28 +53,21 @@ def change_secs():
     with lock:
         if seconds_change == 10:
             seconds_change = 5
-            return
         elif seconds_change == 5:
             seconds_change = 1
-            return
-        seconds_change = 10
-        return
+        else:
+            seconds_change = 10
 def main():
-    global seconds_change
-    run_thread()
-
-    while(True):
-        switch.update()
-        if switch.fell == True:
-            with lock:
-                if seconds_change == 10:
-                    seconds_change = 5
-                elif seconds_change == 5:
-                    seconds_change = 1
-                else:
-                    seconds_change = 10
-            time.sleep(0.5)
-    
+    global stop_thread
+    try:
+        run_thread()
+        while(True):
+            switch.update()
+            if switch.fell == True:
+                change_secs()
+                time.sleep(0.5)
+    except KeyboardInterrupt:
+        stop_thread = True
      
 
 
