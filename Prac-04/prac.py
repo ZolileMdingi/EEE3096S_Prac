be0b3f3bef8e73998ac4105f1f4f2617f25c3608
ZolileMdingi <50634614+ZolileMdingi@users.noreply.github.com>
2021-10-07 22:07:43 +0200

    fixed minor buf

--- Prac-04/prac.py
+++ Prac-04/prac.py
@@ -40,9 +40,9 @@ def app(temp_channel, ldr_channel):
     global stop_thread
     second = 0
     init_second_val = time.time()
-    print("{} \t {} \t {}   \t {}".format("Runtime","Temp Reading","Temp","Light Reading"))
+    print("{:<7} \t {:<12} \t {:<4}   \t {:<12}".format("Runtime","Temp Reading","Temp","Light Reading"))
     while not stop_thread:
-        print(str(round(second)).ljust(6),'\t','{:<9.2f}'.format(temp_channel.value),'\t','{:<4.2f}'.format(temp_in_C(temp_channel.voltage)),'C \t',"{:<9.2f}".format(ldr_channel.value))       
+        print((str(round(second))+'s').ljust(6),'\t','{:<9.2f}'.format(temp_channel.value),'\t','{:<4.2f}'.format(temp_in_C(temp_channel.voltage)),'C \t',"{:<9.2f}".format(ldr_channel.value))       
         # print("{runningtime:<}s \t {temp_reading:<9.2f} \t {temmp:<4.2f} C \t {l_reading:<9.2f}".format(runningtime = round(second),temp_reading = temp_channel.value,temmp = temp_in_C(temp_channel.value,temp_channel.voltage),l_reading  = ldr_channel.value))
         with lock:
             time.sleep(seconds_change)
