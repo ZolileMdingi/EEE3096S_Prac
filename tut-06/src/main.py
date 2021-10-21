4e48c93855d57a70a99fe59aacd2dd33e2050f27
Wavhudi Mulaudzi <mulaudziwavhudi@gmail.com>
2021-10-22 01:05:06 +0200

    Impoved the text

--- tut-06/src/main.py
+++ tut-06/src/main.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World! Meet Wavhudi(MLDWAV001) and Zolile(MDNAVE001)'
+    return 'Hello World! Meet 👨‍💻Wavhudi(MLDWAV001) and 👨‍💻Zolile(MDNAVE001)'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
